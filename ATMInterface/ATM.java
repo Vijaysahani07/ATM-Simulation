e39207cb41765bc39fe99ad0b16285532acd6c46
Vijay Sahani <sahani0vijay@gmail.com>
2025-03-27 19:12:32 +0000

    deleted

--- ATMInterface/ATM.java
+++ ATMInterface/ATM.java
@@ -1,54 +1,89 @@
-// Source code is decompiled from a .class file using FernFlower decompiler.
-import java.util.Scanner;
-
-public class atm {
-   public atm() {
-   }
-
-   public static void main(String[] var0) {
-      int var1 = 100000;
-      
-      Scanner var4 = new Scanner(System.in);
-
-      while(true) {
-         while(true) {
-            System.out.println("Automated Teller Machine");
-            System.out.println("Choose 1 for Withdraw");
-            System.out.println("Choose 2 for Deposit");
-            System.out.println("Choose 3 for Check Balance");
-            System.out.println("Choose 4 for EXIT");
-            System.out.print("Choose the operation you want to perform:");
-            int var5 = var4.nextInt();
-            switch (var5) {
-               case 1:
-                  System.out.print("Enter money to be withdrawn:");
-                  int var2 = var4.nextInt();
-                  if (var1 >= var2) {
-                     var1 -= var2;
-                     System.out.println("Please collect your money");
-                  } else {
-                     System.out.println("Insufficient Balance");
-                  }
-
-                  System.out.println("");
+//import required classes and packages   
+import java.util.Scanner;  
+  
+//create ATMExample class to implement the ATM functionality  
+public class atm 
+{  
+  public static boolean validateInputAmount(Integer amount)
+  {
+    return amount>0;
+  }
+    //main method starts   
+    public static void main(String args[] )  
+    {  
+        //declare and initialize balance, withdraw, and deposit  
+        int balance = 100000, withdraw, deposit;  
+          
+        //create scanner class object to get choice of user  
+        Scanner sc = new Scanner(System.in);  
+          
+        while(true)  
+        {  
+            System.out.println("Automated Teller Machine");  
+            System.out.println("Choose 1 for Withdraw");  
+            System.out.println("Choose 2 for Deposit");  
+            System.out.println("Choose 3 for Check Balance");  
+            System.out.println("Choose 4 for EXIT");  
+            System.out.print("Choose the operation you want to perform:");  
+              
+            //get choice from user  
+            int choice = sc.nextInt();  
+            switch(choice)  
+            {  
+                case 1:  
+        System.out.print("Enter money to be withdrawn:");  
+                      
+        //get the withdrawl money from user  
+        withdraw = sc.nextInt(); 
+                if(!validateInputAmount(withdraw))
+                {
+                  System.out.println("Enter amount greater than zero");
                   break;
-
-               case 2:
-                  System.out.print("Enter money to be deposited:");
-                  int var3 = var4.nextInt();
-                  var1 += var3;
-                  System.out.println("Your Money has been successfully depsited");
-                  System.out.println("");
+                }
+                      
+        //check whether the balance is greater than or equal to the withdrawal amount  
+        if(balance >= withdraw)  
+        {  
+            //remove the withdrawl amount from the total balance  
+            balance = balance - withdraw;  
+            System.out.println("Please collect your money");  
+        }  
+        else  
+        {  
+            //show custom error message   
+            System.out.println("Insufficient Balance");  
+        }  
+        System.out.println("");  
+        break;  
+   
+                case 2:  
+                      
+        System.out.print("Enter money to be deposited:");  
+                      
+        //get deposite amount from te user  
+        deposit = sc.nextInt();  
+                 if(!validateInputAmount(deposit))
+                {
+                  System.out.println("Enter amount greater than zero");
                   break;
-
-               case 3:
-                  System.out.println("Balance : " + var1);
-                  System.out.println("");
-                  break;
-               case 4:
-                  System.exit(0);
-            }
-         }
-      }
-   }
-}
+                }
+                      
+        //add the deposit amount to the total balanace  
+        balance = balance + deposit;  
+        System.out.println("Your Money has been successfully depsited");  
+        System.out.println("");  
+        break;  
+   
+                case 3:  
+        //displaying the total balance of the user  
+        System.out.println("Balance : "+balance);  
+        System.out.println("");  
+        break;  
+   
+                case 4:  
+        //exit from the menu  
+        System.exit(0);  
+            }  
+        }  
+    }  
+}  
